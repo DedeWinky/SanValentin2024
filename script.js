23387440500a677af7dd3277255e0d057d6dff75
DedeWinky <160018042+DedeWinky@users.noreply.github.com>
2024-02-14 20:42:49 +0100

    Flores

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-function checkPassword() {
-    var enteredDate = document.getElementById('dateInput').value;
-    var correctDate = '2022-11-07'; // Nueva fecha deseada en el formato YYYY-MM-DD
-  
-    if (enteredDate === correctDate) {
-      document.getElementById('message').innerHTML = 'Fecha correcta. Redireccionando...';
-      // Redirecciona a la página deseada después de ingresar la fecha
-      window.location.href = './Flores/index.html'; // Cambia 'otra_pagina.html' por la URL de la página a la que deseas redirigir
-    } else {
-      document.getElementById('message').innerHTML = 'Fecha incorrecta. Por favor, intenta de nuevo.';
-    }
+onload = () => {
+    document.body.classList.remove("container");
+  };
+
+  function redirectToPage() {
+    window.location.href = '../Carta/index.html'; // Cambia 'otra_pagina_local.html' por la URL de la página local a la que deseas redirigir
   }
