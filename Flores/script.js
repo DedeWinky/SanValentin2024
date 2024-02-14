34ee3f1afc22f001b03d663fce59dd332ed6f4d0
DedeWinky <160018042+DedeWinky@users.noreply.github.com>
2024-02-14 20:48:58 +0100

    Add files via upload

---
+++ Flores/script.js
@@ -0,0 +1,7 @@
+onload = () => {
+    document.body.classList.remove("container");
+  };
+
+  function redirectToPage() {
+    window.location.href = '../Carta/index.html'; // Cambia 'otra_pagina_local.html' por la URL de la página local a la que deseas redirigir
+  }
